5bae8ec3a815aab4dcaeffd271bbf2ca946317ab
franciscoRoz <francisco.rozas@outlook.com>
2021-08-15 20:38:51 -0400

    sdasd

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const app = express();
+
 const router = express.Router();
 const { sendToDialogFlow } = require("./DialogFlow/DialogFlow");
 
-app.get("/", function (req, res) {
+router.get("/", function (req, res) {
   res.send("Hello World");
 });
 
@@ -17,7 +17,5 @@ router.post("/webhook", express.json(), function (req, res) {
   res.send(resDF).status(200) 
 });
 
-let port = 3000;
-router.listen(port, () => {
-  console.log("Estamos ejecutando el servidor en el puerto " + port);
-});
+
+module.exports = router;
