c74e0c063b1b018c097a71bacadd795c706e4224
franciscoRoz <francisco.rozas@outlook.com>
2021-11-06 23:56:13 -0300

    push usuarios

---
+++ Querys-BD/Qerys/Usuarios.js
@@ -0,0 +1,121 @@
+const { ejecutarqery } = require("../dboperaciones.js");
+
+let infousuario = async (email) => {
+  try {
+    let res = await ejecutarqery(`
+        SELECT TOP (1) gpn,email,nombre,apellido,estado,tipopermiso 
+        FROM [dbo].[USUARIO]
+        LEFT JOIN [dbo].[TIPO_PERMISO] 
+        ON USUARIO.idpermiso=TIPO_PERMISO.idpermiso
+        WHERE email = '${email}'`);
+    return evaluarespuesta(res[0][0]);
+  } catch {
+    console.log(`Error al obtener la información del usuario: ${email}`);
+  }
+};
+
+let validarusuario = async (email, pass) => {
+  try {
+    let res = await ejecutarqery(`
+        SELECT TOP (1) gpn,email,nombre,apellido,estado,tipopermiso 
+        FROM [dbo].[USUARIO] 
+        LEFT JOIN [dbo].[TIPO_PERMISO] 
+        ON USUARIO.idpermiso=TIPO_PERMISO.idpermiso
+        WHERE email = '${email}' and PWDCOMPARE('${pass}', clave)= 1`);
+
+    return evaluarespuesta(res[0][0]);
+  } catch {
+    console.log("error al validar el usuario");
+  }
+};
+
+let insertarusuario = async (
+  gpn,
+  email,
+  clave,
+  nombre,
+  apellido,
+  idpermiso
+) => {
+  try {
+    let res = await ejecutarqery(`
+      INSERT INTO [dbo].[USUARIO]
+      ([gpn]
+      ,[email]
+      ,[clave]
+      ,[nombre]
+      ,[apellido]
+      ,[estado]
+      ,[idpermiso])
+VALUES
+      ('${gpn}'
+      ,'${email}'
+      ,PWDENCRYPT('${clave}')
+      ,'${nombre}'
+      ,'${apellido}'
+      ,'activo'
+      ,${idpermiso})`);
+
+    return "";
+  } catch {
+    console.log("error al registrar el usuario");
+  }
+};
+
+let obteneridpermiso = async (permiso) => {
+  try {
+    let res = await ejecutarqery(`
+        SELECT TOP (1) [idpermiso]
+        FROM [dbo].[TIPO_PERMISO]
+        where tipopermiso='${permiso}'`);
+    return evaluarespuesta(res[0][0]);
+  } catch {
+    console.log(`No se encontró el permiso ${permiso}`);
+  }
+};
+
+let existeusuario = async (gpn) => {
+  try {
+    let res = await ejecutarqery(`
+        SELECT TOP (1) gpn,email,nombre,apellido,estado,tipopermiso 
+        FROM [dbo].[USUARIO]
+        LEFT JOIN [dbo].[TIPO_PERMISO] 
+        ON USUARIO.idpermiso=TIPO_PERMISO.idpermiso
+        WHERE gpn = '${gpn}'`);
+    return evaluarespuesta(res[0][0]);
+  } catch {
+    console.log(`Error al obtener la información del usuario: ${email}`);
+  }
+};
+
+let actualizarclave = async (gpn, clave, email, nuevaclave) => {
+  try {
+    let res = await ejecutarqery(`
+    UPDATE [dbo].[USUARIO]
+    SET [clave] = PWDENCRYPT('${nuevaclave}')
+    WHERE email='${email}' 
+    and gpn='${gpn}' 
+    and PWDCOMPARE('${clave}', clave)= 1`);
+  } catch {
+    console.log(
+      `error al ejecutar la actualizacion de clave del usuario: ${email}`
+    );
+  }
+};
+
+let evaluarespuesta = (res) => {
+  if (res === undefined) {
+    return "";
+  } else {
+    return res;
+  }
+};
+
+module.exports = {
+  infousuario,
+  validarusuario,
+  obteneridpermiso,
+  insertarusuario,
+  existeusuario,
+  actualizarclave,
+};
---
+++ Usuario/ApiUsuario.js
@@ -0,0 +1,113 @@
+const { Express } = require("actions-on-google/dist/framework/express");
+const express = require("express");
+const {
+  infousuario,
+  validarusuario,
+  obteneridpermiso,
+  existeusuario,
+  insertarusuario,
+  actualizarclave,
+} = require("../Querys-BD/Qerys/Usuarios");
+
+const router = express.Router();
+
+router.get("/info/:id", express.json(), async function (req, res) {
+  try {
+    let { id } = req.params;
+
+    let info = await infousuario(id);
+
+    enviarinfo(info, "Correo no encontrado, inténtelo más tarde", res);
+  } catch {
+    res
+      .send(
+        "Error al consultar la información del usuario validar el correo ingresado"
+      )
+      .status(400);
+  }
+});
+
+router.post("/validarusuario", express.json(), async function (req, res) {
+  try {
+    console.log("entre");
+    let { email, pass } = req.body[0];
+
+    let resqery = await validarusuario(email, pass);
+
+    enviarinfo(resqery, "Correo o contraseña no válidos", res);
+  } catch {
+    res
+      .send(
+        "Error al consultar la información del usuario validar el correo ingresado"
+      )
+      .status(400);
+  }
+});
+
+router.post("/crearusuario", express.json(), async function (req, res) {
+  try {
+    let { gpn, email, clave, nombre, apellido } = req.body[0];
+
+    let validaruser = await existeusuario(gpn);
+
+    validaruser !== "" ? res.send("Usuario ya registrado").status(400) : null;
+
+    let idpermiso = await obteneridpermiso("usuario");
+
+    idpermiso === ""
+      ? res
+          .send("Ha ocurrido un error al crear el usuario, inténtelo más tarde")
+          .status(400)
+      : null;
+
+    let crearusuario = await insertarusuario(
+      gpn,
+      email,
+      clave,
+      nombre,
+      apellido,
+      idpermiso.idpermiso
+    );
+
+    validaruser = await existeusuario(gpn);
+
+    validaruser !== ""
+      ? res.send("OK").status(200)
+      : res
+          .send("Ha ocurrido un error al crear el usuario, inténtelo más tarde")
+          .status(400);
+  } catch {
+    res
+      .send("Ha ocurrido un error al crear el usuario, inténtelo más tarde1")
+      .status(400);
+  }
+});
+
+router.post("/cambiarclave", express.json(), async function (req, res) {
+  let { gpn, email, clave, nuevaclave } = req.body[0];
+
+  let validacionclave = await validarusuario(email, clave);
+
+  validacionclave === "" ? res.send("clave no valida").status(400) : null;
+
+  let actualizarcontraseña = await actualizarclave(
+    gpn,
+    clave,
+    email,
+    nuevaclave
+  );
+
+  validacionclave = await validarusuario(email, nuevaclave);
+
+  validacionclave === ""
+    ? res
+        .send(
+          "Error en cambio de clave ,en caso de de algun problema porfavor contactar a su consulour"
+        )
+        .status(400)
+    : res.send("OK").status(200);
+});
+let enviarinfo = (resqery, err, res) => {
+  resqery == "" ? res.send(err).status(400) : res.send(resqery).status(200);
+};
+module.exports = router;
