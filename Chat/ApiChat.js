0182426ee94baf42349920377ee4b638e908efac
franciscoRoz <francisco.rozas@outlook.com>
2021-11-19 23:40:40 -0300

    make it better

---
+++ Chat/ApiChat.js
@@ -0,0 +1,35 @@
+const { sendToDialogFlow } = require("./DialogFlow");
+const express = require("express");
+const { insertarmensaje, obtenermensajes } = require("../Querys-BD/Qerys/Chat");
+const router = express.Router();
+router.post("/enviarmensaje", express.json(), async function (req, res) {
+  let { msg, gpn, source } = req.body[0];
+
+  let resDF = await sendToDialogFlow(msg, gpn, source, "");
+
+  const { text } = resDF.fulfillmentMessages[0].text;
+
+  res.send(text).status(200);
+});
+
+router.post("/respaldarmensaje", express.json(), async function (req, res) {
+  let { msg, gpn, fecha } = req.body[0];
+
+  let respuestainsertdato = await insertarmensaje(gpn, msg, fecha);
+
+  respuestainsertdato === ""
+    ? res.send("ok").status(200)
+    : res.send("*no se pudo enviar tu mensaje").status(400);
+});
+
+router.get("/obtenerhistorico/:gpn", express.json(), async function (req, res) {
+  let { gpn } = req.params;
+
+  let Mensajeshistoricos = await obtenermensajes(gpn);
+
+  Mensajeshistoricos !== ""
+    ? res.send(Mensajeshistoricos).status(200)
+    : res.send({}).status(400);
+});
+
+module.exports = router;
---
+++ Querys-BD/Qerys/Chat.js
@@ -0,0 +1,28 @@
+const { ejecutarqery } = require("../dboperaciones");
+let insertarmensaje = async (gpn, msg, fecha) => {
+  try {
+      console.log( `INSERT INTO [dbo].[CHAT] ([gpn],[fecha],[mensaje])
+      VALUES('${gpn}','${fecha}','${msg}')`);
+    await ejecutarqery(
+      `INSERT INTO [dbo].[CHAT] ([gpn],[fecha],[mensaje])
+        VALUES('${gpn}','${fecha}','${msg}')`
+    );
+
+    return "";
+  } catch {
+    console.log("error al cargar el mensaje");
+  }
+};
+let obtenermensajes = async (gpn) => {
+  try {
+    let data=await ejecutarqery(
+      `SELECT [gpn],[fecha],[mensaje] FROM [dbo].[CHAT] where gpn = '${gpn}'`
+    );
+
+    return data[0];
+  } catch {
+    console.log("error al obtener los mensajes");
+  }
+};
+
+module.exports = { insertarmensaje, obtenermensajes };
